15dd359f0892fd01796f213d7b7db7e2576449c4
Julien LIOTTIER <julienliottier@gmail.com>
2018-09-26 15:25:23 +0200

    respect des normes PSR

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -20,24 +20,31 @@ class TemplateManager
         $APPLICATION_CONTEXT = ApplicationContext::getInstance();
 
         $quote = (isset($data['quote']) and $data['quote'] instanceof Quote) ? $data['quote'] : null;
-        $_user  = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : $APPLICATION_CONTEXT->getCurrentUser();
+        $user  = (isset($data['user'])  and ($data['user']  instanceof User))
+                 ? $data['user']
+                 : $APPLICATION_CONTEXT->getCurrentUser();
 
         if ($quote)
         {
-            $_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
+            $quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
             $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
             $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
 
-            $text = replace($text, 'quote', 'summary_html', Quote::renderHtml($_quoteFromRepository));
+            $text = replace($text, 'quote', 'summary_html', Quote::renderHtml($quoteFromRepository));
 
-            $text = replace($text, 'quote', 'summary', Quote::renderText($_quoteFromRepository));
+            $text = replace($text, 'quote', 'summary', Quote::renderText($quoteFromRepository));
 
             $text = replace($text, 'quote', 'destination_name', $destinationOfQuote->countryName);
 
-            $text = replace($text, 'quote', 'destination_link', $usefulObject->url . '/' . $destinationOfQuote->countryName . '/quote/' . $_quoteFromRepository->id);
+            $text = replace(
+                $text,
+                'quote',
+                'destination_link',
+                $usefulObject->url . '/' . $destinationOfQuote->countryName . '/quote/' . $quoteFromRepository->id
+            );
         }       
     
-        $text = replace($text, 'user', 'first_name', ucfirst(mb_strtolower($_user->firstname)));
+        $text = replace($text, 'user', 'first_name', ucfirst(mb_strtolower($user->firstname)));
 
         return $text;
     }
